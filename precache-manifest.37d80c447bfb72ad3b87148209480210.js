badc3aff781592fdf93442de50d1877a7ed569fe
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-11-25 22:19:42 +0530

    deploy

---
+++ precache-manifest.37d80c447bfb72ad3b87148209480210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a71dc5c89209ad94d0c",
+    "url": "/vue-todo/css/app.4639eb72.css"
+  },
+  {
+    "revision": "1251d2cf27c41961e2d12c1059d8b4c3",
+    "url": "/vue-todo/index.html"
+  },
+  {
+    "revision": "3a71dc5c89209ad94d0c",
+    "url": "/vue-todo/js/app.0f702198.js"
+  },
+  {
+    "revision": "2340aa423633c69b4705",
+    "url": "/vue-todo/js/chunk-vendors.04815c08.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "/vue-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-todo/robots.txt"
+  }
+]);
